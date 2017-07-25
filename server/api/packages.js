7a3d27f945262736d7a0bd10530d9313b1c4c4ac
Camden Ko <camdenko@gmail.com>
2017-07-25 14:34:58 -0500

    refactor index packages

--- server/api/packages.js
+++ server/api/packages.js
@@ -4,32 +4,43 @@ module.exports = router
 
 router.get('/', (req, res, next) => {
   Package.findAll({})
-   .then(users => res.json(users))
+   .then(packages => res.json(packages))
    .catch(next);
 })
 
-router.get('/:packageId',(req, res, next)=>{
-  Package.findOne({where:{ id: req.params.packageId }})
-  .then(package => res.json(package))
-  .catch(next);
+router.param('packageId', (req, res, next, packageId) => {
+  Package.findById(packageId)
+  .then(pkg => {
+    if (!pkg){
+      const err = Error('Package not found.')
+      err.status = 404
+      throw err
+    }
+
+    req.pkg = pkg
+    next()
+  })
+  .catch(next)
 })
 
-router.post('/', (req, res, next)=>{
-  Package.create(req.body)
-  .then(package => res.json(package))
-  .catch(next);
+router.get('/:packageId', (req, res, next) => {
+  res.json(req.pkg)
 })
 
-router.put('/:packageId',(req,res,next)=>{
-  Package.findOne({where:{ id: req.params.packageId }})
-  .then(package => package.update(req.body))
+router.put('/:packageId', (req, res, next) => {
+  req.pkg.update(req.body)
   .then(result => res.status(201).json(result))
-  .catch(next);    
+  .catch(next);
 })
 
 router.delete("/:packageId", (req, res, next) => {
-  Package.findById(req.params.packageId)
-  .then( package => package.destroy({}) )
+  req.pkg.destroy({})
   .then( result => res.sendStatus(204))
+  .catch(next);
+})
+
+router.post('/', (req, res, next) => {
+  Package.create(req.body)
+  .then(pkg => res.json(pkg))
   .catch(next);
 })
