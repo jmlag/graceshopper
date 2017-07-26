4fc9669f10219fa8d64719e06c3fccfd4cdb5b10
Camden Ko <camdenko@gmail.com>
2017-07-26 17:30:55 -0500

    cart split into two columns

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,12 +1,81 @@
-import React, { Component } from "react";
+import React from 'react'
+import { connect } from 'react-redux'
 
-export default class Cart extends Component {
-    render() {
-        return (
-            <div>
-                SHOPPING CART COMPONENT
-            </div>
-        );
-    }
+//map half the cart items to one column and the other half to the other
+function Cart(props){
+  return (
+    <div className="container">
+      <br /><br />
+      <h1 className="header center mainColor-text">Cart</h1>
+      <br /><br />
+      <div className="row">
+        <div className="col s12 m6">
+          <ul className="collection">
+            {
+            props.cart.slice(0, Math.round(props.cart.length / 2)).map(pkg => (
+            <li key={pkg.id} className="collection-item avatar">
+              <img src = {pkg.image} className="circle" />
+              <span className="collection-title">{pkg.name}</span>
+              <p>
+                ${pkg.price}
+              </p>
+            </li>))
+            props.cart.slice(Math.round(props.cart.length / 2)).map(pkg => (
+
+            ))
+            }
+          </ul>
+        </div>
+      </div>
+      <div className="row center">
+        <div className="col s12 m6">
+          <ul className="collapsible" data-collapsible="accordion">
+            <li>
+              <div className = "collapsible-header">FIRST</div>
+            </li>
+          </ul>
+        </div>
+        <div className="col s12 m6">
+          <ul className="collapsible" data-collapsible="accordion">
+            <li>
+              <div className = "collapsible-header">SECOND</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
+function mapStateToProps(state) {
+  return {
+    // cart: state.cart,
+    cart: [{
+      name: "SHIRT",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg",
+      price: 100,
+    },{
+      name: "SHIRT",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg",
+      price: 100,
+    },{
+      name: "SHIRT",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg",
+      price: 100,
+    },{
+      name: "SHIRT",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg",
+      price: 100,
+    },{
+      name: "SHIRT",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg",
+      price: 100,
+    },{
+      name: "SHIRT",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d2/Jeans_for_men.jpg",
+      price: 100,
+    },]
+  }
+}
+
+export default connect(mapStateToProps)(Cart)
