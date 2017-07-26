3384883f2acfaa67618d5653a1ac0f7a5e95800c
Camden Ko <camdenko@gmail.com>
2017-07-25 20:00:00 -0500

    init work

--- client/components/PackageList/index.js
+++ client/components/PackageList/index.js
@@ -3,26 +3,28 @@ import { connect } from 'react-redux';
 import { withRouter, NavLink } from 'react-router-dom';
 import Product from "./Product";
 
-function ProductsList (props) {
+function PackageList (props) {
 
-  return (<div>  
-    Products list component  
-
+  return (<div>
+    Packages list component
+    {
+      props.packages.map(pkg => <Package pkg = {pkg} />)
+    }
     <NavLink to="/packages/1" >
-    <Product 
-      thumbnail={true} 
+    <Product
+      thumbnail={true}
       product={{
         name: "prod1",
         image: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
         price: "123456",
         description: "product 1 description",
-      }} 
+      }}
     />
-    </ NavLink>
+    </NavLink>
 
     <NavLink to="/packages/2" >
-    <Product 
-      thumbnail={true} 
+    <Product
+      thumbnail={true}
       product={{
         name: "prod2",
         image: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
@@ -30,39 +32,14 @@ function ProductsList (props) {
         description: "product 2 description",
       }}
     />
-    </ NavLink>
+    </NavLink>
 
-    </div>); 
-
-  //const { products } = props;
-
-  // return (
-  //   <div>   
-  //       {/* searchbar and dropdown filter component  */}
-          
-  //     <ul>
-  //       { 
-  //         products
-  //         .filter( message => {
-  //           //filter parameters
-  //         })
-  //         .map(message => (
-  //           <li key={message.id} >
-  //             <NavLink to={/* api route */}> 
-  //               {/* product thumbnail component - basically <Product /> without discription and smaller image */}
-  //             </ NavLink>
-  //           </li>
-  //         ))
-  //       }
-  //     </ul>
-
-  //   </div>
-  // );
+    </div>);
 }
 
 const mapStateToProps = function (state, ownProps) {
   return {
-    products: state.products
+    packages: state.packages,
   };
 };
 
@@ -73,4 +50,4 @@ const mapDispatchToProps = function (dispatch) {
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(ProductsList);
+)(PackageList);
