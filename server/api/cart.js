3fb1dc2e3393575f28b89d9b70e19f295d7c8f1c
Camden Ko <camdenko@gmail.com>
2017-07-26 10:22:26 -0500

    added router.use

--- server/api/cart.js
+++ server/api/cart.js
@@ -5,35 +5,35 @@ const router = require('express').Router()
 const { Cart } = require('../db/models')
 module.exports = router
 
-router.use((res, res, next) => {
-  Cart.findOne({where: {
-    userId: req.user.id,
-  }})
-  .then(cart => {
-    req.cart = cart
-    next()
-  })
-
-})
-
-router.get('/', (req, res, next) => {
-  if(req.user) {
-
-    .then(cart => {res.status(200).json(cart)})
-    .catch(next)
-  } else {
-    res.sendStatus(204)
-  }
-})
-//how do i make this drier
-router.put('/', (req, res, next) => {
+router.use((req, res, next) => {
   if(req.user) {
     Cart.findOne({where: {
       userId: req.user.id,
     }})
-    .then(cart => {res.status(200).json(cart)})
+    .then(cart => {
+      req.cart = cart
+      next()
+    })
     .catch(next)
   } else {
     res.sendStatus(204)
   }
+
+})
+
+router.get('/', (req, res, next) => {
+  res.status(200).json(req.cart)
+})
+
+//how do i make this drier
+router.put('/', (req, res, next) => {
+  req.cart.update(req.body)
+  .then(updatedCart => res.status(200).json(updatedCart))
+  .catch(next)
+})
+
+router.delete('/', (req, res, next) => {
+  req.cart.destroy()
+  .then(res.sendStatus(204))
+  .catch(next)
 })
